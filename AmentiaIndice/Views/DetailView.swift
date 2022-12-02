d70fabd5232a879deecd22c12d1992a353a3e18c
Losarkreator <losarkreator@gmail.com>
2022-12-02 16:47:36 +0100

    end editing

--- AmentiaIndice/Views/DetailView.swift
+++ AmentiaIndice/Views/DetailView.swift
@@ -10,20 +10,35 @@ import SwiftUI
 struct DetailView: View {
     var item: Capitulo
     @EnvironmentObject var model : ViewModel
-    @State private var isShowingViewBottom = false
     @State private var textoDescripcion = ""
-    @State private var fullText: String = "This is some editable text..."
+    @FocusState private var nameIsFocused: Bool
+    //@State private var isShowingViewBottom = false
+    //@State private var endEdit = false
     
     var body: some View {
         VStack {
             TextEditor(text: $textoDescripcion)
+                .focused($nameIsFocused)
+            //.colorMultiply(.yellow)
+            //.onChange(of: textoDescripcion, perform: <#T##(Equatable) -> Void##(Equatable) -> Void##(_ newValue: Equatable) -> Void#>)
             
-            Button(action: {
-                sendText()
-            }, label: {
-                Image(systemName: "paperplane")
-                Text("Guardar")
-            })
+            if nameIsFocused {
+                Button(action: {
+                    sendText()
+                    nameIsFocused = false
+                    //                endEdit = true
+                }, label: {
+                    Image(systemName: "paperplane")
+                    Text("Guardar")
+                })
+//                .alert(
+//                    "Se ha guardado correctamente",
+//                    isPresented: $textoDescripcion) {
+//                        Button("OK", role: .cancel) { }
+//                    }
+            }
+            
+            
             
         }
         .onAppear{
@@ -39,22 +54,10 @@ struct DetailView: View {
 
 //MARK: - Funciones
 extension DetailView {
-    func switchButtonsView() {
-        isShowingViewBottom = !isShowingViewBottom
-    }
-    
+
     func sendText() {
         model.updateData(capToUpdate: item, newText: textoDescripcion)
         model.getData()
-        //textoDescripcion = ""
-        //MARK: - Alert
-        /*
-         Añadir Alert para decir que se ha guardado
-         Cerrar el modo edicion del text editor:
-            Dejar de editar
-            Cerrar teclado
-         */
-        
     }
     
 }
@@ -62,50 +65,61 @@ extension DetailView {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        
-        //        DetailView(item: ViewModel().list[2])
-        
+        //DetailView(item: ViewModel().list[2])
         DetailView(item: Capitulo.init(id: "dsadsa",
                                        nombre: "Capitulo 1",
                                        orden: 1, descripcion: "Test text. \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n Fin")
         )
-        
-        
     }
+}
+
+
+//MARK: Alert
+extension DetailView {
+    //MARK: - Alert
+    // https://youtu.be/xG9v4llPR3Y?t=236
+    /*
+     Alert(
+     title: Text("Se ha guardado correctamente"),
+     message: Text("Wear sunscreen"),
+     dismissButton: .default(Text("OK"))
+     )
+     */
 }
 
 
 //MARK: Seguridad
 extension DetailView {
-    
-    //            ScrollView {
-    //                // Cambiar texto para que pueda ser editado:
-    //                Text(item.descripcion)
-    //                    .font(.body)
-    //                    .frame(maxWidth: .infinity)
-    //               // Añadir gesto:
-    ////                    .onLongPressGesture(minimumDuration: 2) //
-    //                    .onTapGesture(count: 2)
-    //                    {
-    //                        switchButtonsView()
-    //                    }
-    //
-    //            }
-    
-    
-    //            if isShowingViewBottom {
-    //                HStack {
-    //                    TextField("Placeholder", text: $textoDescripcion)
-    //                        .textFieldStyle(.roundedBorder)
-    //
-    //                    Button(action: {
-    //                        // Accion
-    //                        sendText()
-    //                    }, label: {
-    //                        Image(systemName: "paperplane")
-    //                        Text("Guardar")
-    //                    })
-    //                }
-    //                .frame(height: 50)
-    //            }
+    /*
+     ScrollView {
+     // Cambiar texto para que pueda ser editado:
+     Text(item.descripcion)
+     .font(.body)
+     .frame(maxWidth: .infinity)
+     // Añadir gesto:
+     //                    .onLongPressGesture(minimumDuration: 2) //
+     .onTapGesture(count: 2)
+     {
+     switchButtonsView()
+     }
+     
+     }
+     
+     
+     if isShowingViewBottom {
+     HStack {
+     TextField("Placeholder", text: $textoDescripcion)
+     .textFieldStyle(.roundedBorder)
+     
+     Button(action: {
+     // Accion
+     sendText()
+     }, label: {
+     Image(systemName: "paperplane")
+     Text("Guardar")
+     })
+     }
+     .frame(height: 50)
+     }
+     */
 }
